c850b6d1b5d1f03f5667fceaad37bfb1bbcf35cf
“Ashish <tentaclebites@gmail.com>
2024-06-29 22:12:10 +0530

    update bcrypt

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,5 +1,5 @@
 const Admin = require('../models/admin');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
 // Making Middleware Functions for Admin Operations.
